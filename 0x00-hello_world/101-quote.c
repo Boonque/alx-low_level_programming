3898b3b75d48734f2687dd4f30399c68bb185851
Boonque <Bboonque@gmail.com>
2022-04-07 18:13:14 -0700

    end

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-09\n", 61);
+
+	return(1);
+}
