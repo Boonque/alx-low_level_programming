612e33722849c929bf29f069a189ed97fac6af7b
Boonque <Bboonque@gmail.com>
2022-04-08 18:29:04 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - main block
- * Description: Use `putchar`to print all letters but 'q'and 'e'.
- * Return: 0
- */
+ *  * main - main block
+ *   * Description: Use `putchar`to print all letters but 'q'and 'e'.
+ *    * Return: 0
+ *     */
 int main(void)
 {
 char c = 'a';
