25f0f4ac5f1d6b0397ae62cfe971df2dfd94ae55
Boonque <Bboonque@gmail.com>
2022-04-08 19:16:12 -0700

    task 8

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 char c;
-for (c = 'z'; c >= 'a';c--)
+for (c = 'z'; c >= 'a'; c--)
 {
 putchar(c);
 }
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base64 in lowercase.
+ * You can only use `putchar`, and only 3 times.
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+
+return (0);
+}
