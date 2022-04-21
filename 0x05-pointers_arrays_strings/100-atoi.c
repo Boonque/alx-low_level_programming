23df2a9598dbd94f02f131e337135ce2e8567f1f
Boonque <Bboonque@gmail.com>
2022-04-20 18:31:18 -0700

    pleaseoh

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -24,10 +24,10 @@ size++;
 }
 count++;
 }
-for(i = count - size; i < count; i++)
+for (i = count - size; i < count; i++)
 {
 oi = oi + ((*(s + i) - 48) * m);
 m /= 10;
 }
-return (oi * pn);
+return (oi *pn);
 }
