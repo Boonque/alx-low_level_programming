79a518d1c78dc6ce230549c07860723e575566e4
Boonque <Bboonque@gmail.com>
2022-04-24 08:42:48 -0700

    task1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,14 +6,15 @@
  * @src: param pointer to a char
  * Return: return value of dest
  */
-
+int main(void)
+{
 char *_strcat(char *dest, char *src)
 {
 int i;
 int j;
 
 i = 0;
-j = 0
+j = 0;
 
 while (dest[i] != '\0')
 {
@@ -26,7 +27,7 @@ dest[i] = src[j];
 j++;
 i++;
 }
-
+}
 dest[i] = '\0';
 return (dest);
-}
+
