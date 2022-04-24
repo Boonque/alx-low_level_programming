79f6e5b9cd74d189fdcb55af7f96d348160d3d48
Boonque <Bboonque@gmail.com>
2022-04-24 12:02:42 -0700

    task9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,7 +2,7 @@
 
 /**
  * print_number - prints an integer.
- * @n: input integer. 
+ * @n: input integer.
  * Return: no return.
  */
 void print_number(int n)
@@ -16,7 +16,11 @@ m = n * -1;
 }
 else
 {
-m + n;
+m = n * -1;
+}
+else
+{
+m = n;
 }
 
 d = m;
@@ -28,7 +32,7 @@ d /= 10;
 count *= 10;
 }
 
-for (; count >= 1; count /= 10)
+for (; count >= 1; count /= 10){
 {
 _putchar(((m / count) % 10) + 48);
 }
