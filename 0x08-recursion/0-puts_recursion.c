6657392adfae12de6940feedd92c95f27b39c80f
Boonque <Bboonque@gmail.com>
2022-04-28 16:37:50 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _puts_recursion - prints a string, followed by a new line.
- * @s: string
- * Return: no return
+ * @s: string.
+ * Return: no return.
  */
 void _puts_recursion(char *s)
 {
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,4 @@
-#ifdef _MAIN_H_
+#ifndef _MAIN_H_
 #define _MAIN_H_
 
 int _putchar(char);
