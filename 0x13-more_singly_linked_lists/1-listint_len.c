3fdac90f81901a2b66c942862f63fac126a5e15d
Boonque <Bboonque@gmail.com>
2022-05-26 10:10:04 -0700

    task1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nnodes = 0;
+size_t nnodes = 0;
 
-	while (h != NULL)
+while (h != NULL)
 
-	{
-		h = h->next;
-		nnodes++;
-	}
-	return (nnodes);
+{
+h = h->next;
+nnodes++;
+}
+return (nnodes);
 }
