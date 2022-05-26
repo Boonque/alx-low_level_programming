9932625ab3e92f2b273210523c4ab6d8912ce146
Boonque <Bboonque@gmail.com>
2022-05-26 10:08:02 -0700

    task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nnodes = 0;
+size_t nnodes = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		nnodes++;
-	}
-	return (nnodes);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+nnodes++;
+}
+return (nnodes);
 }
