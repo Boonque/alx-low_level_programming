76aeba4a51502ef60446a71133c50e03dc1d3bfb
Boonque <Bboonque@gmail.com>
2022-07-13 00:27:44 -0700

    adding .c protypes

--- 0x18-dynamic_libraries/calculate.c
+++ 0x18-dynamic_libraries/calculate.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <unistd.h>
 
 int add(int a, int b)
 {
@@ -20,7 +20,7 @@ int div(int a, int b)
 	return (a / b);
 }
 
-int mod(int a, int b)
+int mod(int a,int b)
 {
 	return (a % b);
 }
