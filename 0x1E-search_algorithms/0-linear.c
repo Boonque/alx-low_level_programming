302dd1301c9fe77a88fad27d4050b425fd055caa
boonque <bboonque@gmail.com>
2022-11-02 15:31:18 -0700

    task 0 update

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,14 +1,14 @@
 #include "search_algos.h"
 
 /**
- *  * linear_search - searches for a value in an array of
- *   * integers using the Linear search algorithm
- *    *
- *     * @array: input array
- *      * @size: size of the array
- *       * @value: value to search in
- *        * Return: Always EXIT_SUCCESS
- *         */
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
+ */
 int linear_search(int *array, size_t size, int value)
 {
 	int i;
